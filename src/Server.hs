a085bbbbb2ff87ccda09313dd0169b4ec9e505d5
Alex Tsvetkov <alextsvetkov@Alexs-MacBook-Pro.local>
2017-11-22 21:13:12 +0300

    Store unparsed input as ShortByteString

--- src/Server.hs
+++ src/Server.hs
@@ -6,6 +6,9 @@ module Server
     ) where
 
 import           Data.Maybe (fromMaybe)
+import           Data.ByteString.Short (ShortByteString)
+import qualified Data.ByteString.Short as ShortBS
+import           Data.ByteString.Char8 (ByteString)
 import qualified Data.ByteString.Char8 as ByteString
 import           Network.Socket hiding (send, sendTo, recv, recvFrom)
 import           Network.Socket.ByteString
@@ -15,6 +18,9 @@ import           Control.Monad.State.Lazy
 
 import           Query
 import qualified Storage
+
+bs :: String -> ByteString
+bs = ByteString.pack
 
 makeSocket :: String -> IO Socket
 makeSocket port = do
@@ -32,16 +38,20 @@ mainLoop sock storage = forever $ do
   putStrLn "Accepting.."
   conn <- accept sock
   putStrLn $ "New connection from " ++ (show $ snd conn)
-  threadId <- forkIO $ evalStateT (handleClient storage conn) $ ByteString.empty
+  threadId <- forkIO $ evalStateT (handleClient storage conn) $ ShortBS.empty
   putStrLn $ "Forked new thread for " ++ (show $ snd conn) ++ " " ++ (show threadId)
 
-handleClient :: Storage.Storage ->  (Socket, SockAddr) -> StateT ByteString.ByteString IO ()
+handleClient :: Storage.Storage ->  (Socket, SockAddr) -> StateT ShortByteString IO ()
 handleClient storage conn@(sock, _)  = do
   input <- lift $ recv sock maxRecv
   if (ByteString.length input /= 0)
     then
     do
-      fullInput <- state $ ByteString.breakEnd (=='\n') . flip ByteString.append input
+      fullInput <- state $
+        fmap (ShortBS.toShort) .
+        ByteString.breakEnd (=='\n') .
+        (flip ByteString.append input) .
+        ShortBS.fromShort
       lift $ do
         forM_ (ByteString.lines fullInput) (
           \line -> do
@@ -58,7 +68,7 @@ handleClient storage conn@(sock, _)  = do
     maxRecv = 1024
 
 
-handleInput :: Storage.Storage -> ByteString.ByteString -> IO ByteString.ByteString
+handleInput :: Storage.Storage -> ByteString -> IO ByteString
 handleInput storage input = do
   case parseQuery input of
     Nothing -> return $ makeError $ "Illegal query: " ++ ByteString.unpack input
@@ -66,11 +76,11 @@ handleInput storage input = do
       result <- query storage q
       return $ result
 
-query :: Storage.Storage -> Query -> IO (ByteString.ByteString)
+query :: Storage.Storage -> Query -> IO (ByteString)
 query storage (Get keys) = formatGet <$> Storage.get storage keys
 query storage (Set key value) = do
   Storage.set storage key value
-  return (ByteString.pack "OK")
+  return (bs "OK")
 query storage (Delete key) = do
   Storage.delete storage key >>= checkResult "Not found"
 query storage (Incr key amount) = checkFound <$> Storage.increment storage key amount
@@ -84,24 +94,24 @@ query storage (Append key value) =
 query storage (Prepend key value) =
   Storage.prepend storage key value >>= checkResult "Not found"
 
-makeError :: String -> ByteString.ByteString
-makeError err = ByteString.pack $ "ERROR " ++ filter (/='\n') err
+makeError :: String -> ByteString
+makeError err = bs $ "ERROR " ++ filter (/='\n') err
 
-checkResult :: String -> Bool -> IO (ByteString.ByteString)
+checkResult :: String -> Bool -> IO (ByteString)
 checkResult err value =
   if value
-    then return $ ByteString.pack "OK"
+    then return $ bs "OK"
     else return $ makeError err
 
-checkFound :: Maybe ByteString.ByteString -> ByteString.ByteString
-checkFound = fromMaybe (ByteString.pack "Not found")
+checkFound :: Maybe ByteString -> ByteString
+checkFound = fromMaybe (bs "Not found")
 
-formatGet :: [(Key, Value)] -> ByteString.ByteString
-formatGet [] = ByteString.pack "Not found"
+formatGet :: [(Key, Value)] -> ByteString
+formatGet [] = bs "Not found"
 formatGet resp =
-  ByteString.intercalate (ByteString.pack "\n") $ map formatItem resp
+  ByteString.intercalate (bs "\n") $ map formatItem resp
   where
-    formatItem :: (Key, Value) -> ByteString.ByteString
+    formatItem :: (Key, Value) -> ByteString
     formatItem (key, value) =
-      let keyStr = ByteString.append (ByteString.pack "VALUE ") key
-      in ByteString.intercalate (ByteString.pack "\n") [keyStr, value, ByteString.pack "END"]
+      let keyStr = ByteString.append (bs "VALUE ") key
+      in ByteString.intercalate (bs "\n") [keyStr, value, ByteString.pack "END"]
