0e5b7c4b95842b39120d0ec4d3cd3306fb985bf1
Alex Tsvetkov <alextsvetkov@Alexs-MacBook-Pro.local>
2017-11-28 20:43:56 +0300

    Delete method for skiplist

--- src/Data/SkipList/IO.hs
+++ src/Data/SkipList/IO.hs
@@ -7,6 +7,7 @@ module Data.SkipList.IO
   , new
   , lookup
   , insert
+  , delete
   ) where
 
 import Prelude hiding (lookup)
@@ -19,9 +20,10 @@ import System.Random (randomIO)
 
 data Node k v = Level {next :: IORef (Node k v), down :: IORef (Node k v)}
               | Head {next :: IORef (Node k v)}
-              | Internal {key :: !k, next :: IORef (Node k v), down :: IORef (Node k v)}
-              | Leaf {key :: !k, value :: IORef v, next :: IORef (Node k v)}
+              | Internal {key :: !k, next :: IORef (Node k v), down :: IORef (Node k v), isDeleted :: IORef Bool}
+              | Leaf {key :: !k, value :: IORef v, next :: IORef (Node k v), isDeleted :: IORef Bool}
               | Nil
+              | Deleted
 
 data SkipList k v = SkipList {headRef :: IORef (Node k v)}
 
@@ -54,29 +56,83 @@ newMaxLevel maxLevel = do
 lookup :: (Ord k) => SkipList k v -> k -> IO (Maybe v)
 lookup SkipList{headRef} k = do
   headTicket <- readForCAS headRef
-  node <- evalStateT (findGTE k) [Start headTicket]
-  case checkNode k node of
-    EQ -> Just <$> readIORef (value node)
-    GT -> return Nothing
-    LT -> error "findGTE returned LT node"
+  res <- evalStateT (findLeafByKey k) [Start headTicket]
+  case res of
+    Just node -> Just <$> readIORef (value node)
+    Nothing -> return Nothing
 
-findGTE :: (Ord k) => k -> StateT (Path k v) IO (Node k v)
-findGTE k = do
+insert :: (Ord k) => SkipList k v -> k -> v -> IO ()
+insert SkipList{headRef} k v = do
+  headTicket <- readForCAS headRef
+  (res, path) <- runStateT (findLeafByKey k) [Start headTicket]
+  case res of
+    Just node -> writeIORef (value node) v
+    Nothing -> evalStateT (insertLeaf k v) path
+
+delete :: (Ord k) => SkipList k v -> k -> IO ()
+delete SkipList{headRef} k = do
+  headTicket <- readForCAS headRef
+  (res, path) <- runStateT (findLeafByKey k) [Start headTicket]
+  case res of
+    Nothing -> return ()
+    Just node -> evalStateT (deleteNode node) path
+
+deleteNode :: (Ord k) => Node k v -> StateT (Path k v) IO ()
+deleteNode node = do
+  levels <- getLevels [node]
+  liftIO $ mapM_ markDeleted levels
+  doDelete (key node)
+    where
+      getLevels :: (Ord k) => [Node k v] -> StateT (Path k v) IO [Node k v]
+      getLevels nodes  = do
+        step <- stepBack
+        case step of
+          Down{node=newNode} -> getLevels (newNode:nodes)
+          _ -> do
+            modify (step:)
+            return nodes
+
+      doDelete :: (Ord k) => k -> StateT (Path k v) IO ()
+      doDelete k = do
+        Next{node=prevNode} <- stepBack
+        ticket <- liftIO $ readForCAS $ (next prevNode)
+        let toDelete = peekTicket ticket
+        del <- liftIO $ deleted toDelete
+        when (del && k==(key toDelete)) $ do
+          nextNode <- liftIO $ readIORef $ next toDelete
+          (success, newTicket) <- liftIO $ casIORef (next prevNode) ticket nextNode
+          liftIO $ writeIORef (next toDelete) Deleted
+          unless (success) $ modify ((Next prevNode newTicket):)
+        case toDelete of
+          Internal{} -> do
+            _ <- backAndDown
+            _ <- forwardToGTE k
+            doDelete k
+          _ -> return ()
+
+findLeafByKey :: (Ord k) => k -> StateT (Path k v) IO (Maybe (Node k v))
+findLeafByKey k = do
+  item <- findByKey k
+  case item of
+    Just node -> do
+      del <- liftIO $ deleted node
+      if del then return Nothing else Just <$> bottom
+    Nothing -> return Nothing
+
+findByKey :: (Ord k) => k -> StateT (Path k v) IO (Maybe (Node k v))
+findByKey k = do
   levelGTE <- forwardToGTE k
-  lastStep <- gets head
-  case node lastStep of
-    Leaf{} -> return levelGTE
-    Head{} -> return levelGTE
-    _ ->
-      case checkNode k levelGTE of
-        EQ -> do
-          t <- liftIO $ readForCAS (next levelGTE)
-          modify((Down levelGTE t):)
-          bottom
-        GT -> do
+  case checkNode k levelGTE of
+    EQ -> return $ Just levelGTE
+    GT -> do
+      lastStep <- gets head
+      case node lastStep of
+        Leaf{} -> return Nothing
+        Head{} -> return Nothing
+        _ -> do
           _ <- backAndDown
-          findGTE k
-        LT -> error "levelGTE returned LT node"
+          findByKey k
+    LT -> error "levelGTE returned LT node"
 
 backAndDown :: StateT (Path k v) IO (Node k v)
 backAndDown = do
@@ -99,8 +155,11 @@ stepBack = state (fromJust . uncons)
 
 bottom :: (Ord k) => StateT (Path k v) IO (Node k v)
 bottom = do
-  downRef <- gets (down . node . head)
-  downNode <- liftIO $ readIORef downRef
+  lastStep <- gets head
+  downNode <- case lastStep of
+    Down{node} -> liftIO $ readIORef (down node)
+    _-> liftIO $ return $ peekTicket . nextTicket $ lastStep
+
   case downNode of
     Internal{} -> do
       t <- liftIO $ readForCAS (next downNode)
@@ -108,15 +167,24 @@ bottom = do
       bottom
     _ -> return downNode
 
-stepForward :: StateT (Path k v) IO (Node k v)
+stepForward :: (Ord k) => StateT (Path k v) IO (Node k v)
 stepForward = do
   prevStep <- gets(head)
   nxt <- liftIO $ case prevStep of
     Down{node} -> readIORef $ down node
     _ -> return $ peekTicket (nextTicket prevStep)
-  nxtTicket <- liftIO $ readForCAS (next nxt)
+  nxtTicket <- liftIO $ nextAlive nxt
   modify ((Next nxt nxtTicket):)
   return (peekTicket nxtTicket)
+  where
+    nextAlive :: (Ord k) => Node k v -> IO (Ticket (Node k v))
+    nextAlive Head{next} = readForCAS next
+    nextAlive Level{next} = readForCAS next
+    nextAlive node = do
+      nextTicket <- readForCAS $ next node
+      let nextNode = peekTicket nextTicket
+      del <- deleted nextNode
+      if del then nextAlive (nextNode) else return nextTicket
 
 forwardToGTE :: (Ord k) => k -> StateT (Path k v) IO (Node k v)
 forwardToGTE k = do
@@ -134,7 +202,7 @@ checkNode k node = compare k (key node)
 insertLeaf :: (Ord k) => k -> v -> StateT (Path k v) IO ()
 insertLeaf k v = do
   Next{node, nextTicket} <- stepBack
-  newNode <- liftIO $ (Leaf k) <$> newIORef v <*> newIORef (peekTicket nextTicket)
+  newNode <- liftIO $ newLeaf k v (peekTicket nextTicket)
   res <- liftIO $ casIORef (next node) nextTicket newNode
   case res of
     (True, _) -> do
@@ -155,7 +223,7 @@ insertLevels base = do
   case step of
     Next{} -> insertLevels base
     Down{node, nextTicket} -> do
-      newNode <- liftIO $ (Internal k) <$> newIORef (peekTicket nextTicket) <*> newIORef base
+      newNode <- liftIO $ newInternal k (peekTicket nextTicket) base
       res <- liftIO $ casIORef (next node) nextTicket newNode
       case res of
         (True, _) -> do
@@ -170,11 +238,19 @@ insertLevels base = do
             LT -> error "forwardToGTE returrned LT node"
     Start{} -> return ()
 
-insert :: (Ord k) => SkipList k v -> k -> v -> IO ()
-insert SkipList {headRef} k v = do
-  headTicket <- readForCAS headRef
-  (node, path) <- runStateT (findGTE k) [Start headTicket]
-  case checkNode k node of
-    GT -> evalStateT (insertLeaf k v) path
-    EQ -> writeIORef (value node) v
-    LT -> error "findGTE returned LT node"
+newInternal :: (Ord k) => k -> Node k v -> Node k v -> IO (Node k v)
+newInternal key next down =
+  (Internal key) <$> newIORef next <*> newIORef down <*> newIORef False
+
+newLeaf :: (Ord k) => k -> v -> Node k v -> IO (Node k v)
+newLeaf key value next =
+  (Leaf key) <$> newIORef value <*> newIORef next <*> newIORef False
+
+markDeleted :: (Ord k) => Node k v -> IO ()
+markDeleted node = writeIORef (isDeleted node) (True)
+
+deleted :: (Ord k) => Node k v -> IO Bool
+deleted Nil = return False
+deleted Head{} = return False
+deleted Level{} = return False
+deleted node = readIORef $ isDeleted node
--- test/Data/SkipList/IOSpec.hs
+++ test/Data/SkipList/IOSpec.hs
@@ -69,6 +69,20 @@ spec = do
         res <- SL.lookup sl "keym"
         res `shouldBe` Just "bar"
 
+      it "should insert smallest value" $ do
+        sl <- SL.new
+        forM_ ['b'..'z'] (\c -> SL.insert sl ("key" ++ [c])  ("value" ++ [c]))
+        SL.insert sl "keya" "bar"
+        res <- SL.lookup sl "keya"
+        res `shouldBe` Just "bar"
+
+      it "should insert biggest value" $ do
+        sl <- SL.new
+        forM_ ['b'..'y'] (\c -> SL.insert sl ("key" ++ [c])  ("value" ++ [c]))
+        SL.insert sl "keyz" "bar"
+        res <- SL.lookup sl "keyz"
+        res `shouldBe` Just "bar"
+
       it "should preserve existing values" $ do
         let assocs = map (\c -> ("key" ++ [c], "value" ++ [c])) $ take 32 ['0'..'z']
         sl <- SL.new
@@ -79,3 +93,40 @@ spec = do
             val <- SL.lookup sl k
             val `shouldBe` Just v
           )
+
+    context "delete" $ do
+      it "should delete value" $ do
+        sl <- SL.new
+        SL.insert sl "foo" "bar"
+        SL.delete sl "foo"
+        res <- SL.lookup sl "foo"
+        res `shouldBe` Nothing
+
+      it "should not interact with other values" $ do
+        let assocs = map (\c -> ("key" ++ [c], "value" ++ [c])) $ take 32 ['0'..'z']
+        sl <- SL.new
+        forM_ assocs $ uncurry $ SL.insert sl
+        SL.delete sl "keyk"
+        res <- SL.lookup sl "keyk"
+        res `shouldBe` Nothing
+        forM_ (filter ((/="keyk") . fst) assocs) (
+          \(k, v) -> do
+            val <- SL.lookup sl k
+            val `shouldBe` Just v
+          )
+
+      it "should delete smallest value" $ do
+        let assocs = map (\c -> ("key" ++ [c], "value" ++ [c])) $ take 32 ['0'..'z']
+        sl <- SL.new
+        forM_ assocs $ uncurry $ SL.insert sl
+        SL.delete sl "keya"
+        res <- SL.lookup sl "keya"
+        res `shouldBe` Nothing
+
+      it "should delete biggest" $ do
+        let assocs = map (\c -> ("key" ++ [c], "value" ++ [c])) $ take 32 ['0'..'z']
+        sl <- SL.new
+        forM_ assocs $ uncurry $ SL.insert sl
+        SL.delete sl "keyz"
+        res <- SL.lookup sl "keya"
+        res `shouldBe` Nothing
